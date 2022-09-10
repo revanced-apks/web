4217a3de6eabfb17d82ad764b556db4f922e190c
Revanced APKs <revanced_apks@rest.eu.org>
2022-09-11 04:11:27 +0530

    Improved UI and API
    - changed api endpoint to raw github file on rate limit grounds
    - improved ui with elegrant new borders
    - remove main red border that looked boring
    - new download button with more rounded corners
    - fix file size calculation

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   const [response, setResponse] = React.useState({});
   React.useEffect(() => {
     fetch(
-      "https://api.github.com/repos/revanced-apks/revanced-apks/releases/latest"
+      "https://raw.githubusercontent.com/revanced-apks/revanced-apks/update/release.json"
     )
       .then((res) => res.json())
       .then((json) => {
@@ -18,31 +18,38 @@ const App = () => {
   }, []);
   return (
     <div className="App flex flex-col items-center justify-center">
-      <header>
-        <h3 className="text-4xl text-red-400 font-extrabold pb-4">
-          ReVanced APKs
+      <header className="border-b-2 w-full pb-4 border-dotted">
+        <h3 className="text-3xl font-extrabold text-red-400">
+         ReVanced Apps
         </h3>
       </header>
-      <div className="border-2 rounded-xl border-red-400 px-2 py-4 w-auto h-100">
-        <div className="flex flex-col md:flex-row md:divide-x md:space-x-5">
-          <div className="flex flex-col space-y-2 items-center">
+      <div className="px-2 py-4 w-auto h-100">
+        <div className="flex flex-col md:flex-row ">
+          <div className="flex flex-col space-y-2 md:pl-3">
             <h1 className="text-2xl text-gray-200 font-extrabold underline pb-3">
               Release
             </h1>
             <h2 className="text-gray-100 text-lg">
               Date:{" "}
-              <span className="font-bold">{response.published_at && new Date(response.published_at).toLocaleString()}</span>
+              <span className="font-bold">
+                {response.published_at &&
+                  new Date(response.published_at).toDateString()}
+              </span>
             </h2>
-            {response.body ? (
-              <p
-                className="text-gray-300 marked font-bold max-w-xl text-xs md:text-base"
-                dangerouslySetInnerHTML={{
-                  __html: marked.parse(response.body.slice(13).replaceAll("\n","<br />")),
-                }}
-              ></p>
-            ) : (
-              <p className="text-gray-300 font-bold">Loading...</p>
-            )}
+            <div className="border p-4 rounded-3xl bg-gray-900 border-gray-400 shadow-md shadow-sky-500">
+              {response.body ? (
+                <p
+                  className="text-gray-300 marked font-bold max-w-xl text-xs md:text-base"
+                  dangerouslySetInnerHTML={{
+                    __html: marked.parse(
+                      response.body.slice(13).replaceAll("\n", "<br />")
+                    ),
+                  }}
+                ></p>
+              ) : (
+                <p className="text-gray-300 font-bold">Loading...</p>
+              )}
+            </div>
             <p className="text-gray-300">
               Use{" "}
               <a href="https://www.apkmirror.com/apk/team-vanced/microg-youtube-vanced/">
@@ -55,7 +62,7 @@ const App = () => {
             <h1 className="text-2xl text-gray-200 font-extrabold underline">
               Downloads
             </h1>
-            <div className="px-2 py-4 space-y-2">
+            <div className="space-y-4">
               {response.assets ? (
                 response.assets
                   .slice()
@@ -66,7 +73,8 @@ const App = () => {
                         key={index}
                         name={asset.name}
                         download={asset.browser_download_url}
-                        size={Number.parseInt(asset.size / 1000000)}
+                        size={Number.parseInt(asset.size)}
+                        download_count={asset.download_count}
                       />
                     );
                   })
@@ -76,34 +84,34 @@ const App = () => {
             </div>
           </div>
         </div>
-        <div className="px-2 py-4">
-          <div className="flex flex-col justify-center items-center pt-5 border px-2 py-4 rounded-3xl bg-gray-900">
+          <div className="flex flex-col justify-center items-center mt-6 border border-gray-400 p-4 rounded-3xl bg-gray-900">
             <a
               className="flex flex-row text-xs text-red-200 no-underline items-center"
               href="https://revanced.app/"
             >
               <img className="h-10 w-10" src={main_icon} alt="Main Logo" />
             </a>
-            <p className="flex items-center justify-center text-gray-100 text-xs">
-              All credits to the original developers of&nbsp;
+            <p className="text-xs">
+              All credits to the original developers of{" "}
               <a
-                className="flex flex-row text-xs text-red-200 no-underline items-center"
+                className="text-xs text-red-200 no-underline"
                 href="https://revanced.app/"
               >
                 ReVanced
               </a>
               .
             </p>
-            <p className="text-xs text-gray-100">
-              Thanks to our upstream repository{" "}
-              <a href="https://github.com/j-hc/revanced-magisk-module">
-                Revanced Magisk Module
+            <p className="pt-2 text-sm">
+              This project is open source and you can contribute on{" "}
+              <a
+                className="text-gray-300"
+                href="https://github.com/revanced-apks"
+              >
+                GitHub
               </a>
-              .
             </p>
           </div>
         </div>
-      </div>
     </div>
   );
 };
